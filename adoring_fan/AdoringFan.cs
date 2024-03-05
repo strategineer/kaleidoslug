6004dc38f19378c13c5568f8f7c452665c787b50
Bill Tyros <me@strategineer.com>
2024-03-05 09:49:24 -0500

    Fix auto reload not working as expected

--- adoring_fan/AdoringFan.cs
+++ adoring_fan/AdoringFan.cs
@@ -4,31 +4,31 @@ using XRL.World;
 
 namespace Strategineer.AdoringFan.PlayerMutators
 {
-    [PlayerMutator]
-    public class MyPlayerMutator : IPlayerMutator
+  [PlayerMutator]
+  public class MyPlayerMutator : IPlayerMutator
+  {
+    public void mutate(GameObject player)
     {
-        public void mutate(GameObject player)
-        {
-            // modify the player object when a New Game begins
-            // for example, add a custom part to the player:
-            //player.AddPart<MyCustomPart>();
-            XRL.Messages.MessageQueue.AddPlayerMessage("Hello, Im a mutate message!");
-        }
+      // modify the player object when a New Game begins
+      // for example, add a custom part to the player:
+      //player.AddPart<MyCustomPart>();
+      XRL.Messages.MessageQueue.AddPlayerMessage("Hello, Im a mutate message!");
     }
+  }
 
-    [HasCallAfterGameLoadedAttribute]
-    public class MyLoadGameHandler
+  [HasCallAfterGameLoadedAttribute]
+  public class MyLoadGameHandler
+  {
+    [CallAfterGameLoadedAttribute]
+    public static void MyLoadGameCallback()
     {
-        [CallAfterGameLoadedAttribute]
-        public static void MyLoadGameCallback()
-        {
-            // Called whenever loading a save game
-            GameObject player = XRLCore.Core?.Game?.Player?.Body;
-            if (player != null)
-            {
-                //player.RequirePart<MyCustomPart>(); //RequirePart will add the part only if the player doesn't already have it. This ensures your part only gets added once, even after multiple save loads.
-                XRL.Messages.MessageQueue.AddPlayerMessage("Hello, Im a message!");
-            }
-        }
+      // Called whenever loading a save game
+      GameObject player = XRLCore.Core?.Game?.Player?.Body;
+      if (player != null)
+      {
+        //player.RequirePart<MyCustomPart>(); //RequirePart will add the part only if the player doesn't already have it. This ensures your part only gets added once, even after multiple save loads.
+        XRL.Messages.MessageQueue.AddPlayerMessage("Hello, Im a message!");
+      }
     }
+  }
 }
--- unlimited_camps/UnlimitedCamps.cs
+++ unlimited_camps/UnlimitedCamps.cs
@@ -5,84 +5,84 @@ using XRL.World.Parts.Skill;
 
 namespace Strategineer.UnlimitedCamps.HarmonyPatches
 {
-    [HarmonyPatch(typeof(Survival_Camp))]
-    class YourPatch1
+  [HarmonyPatch(typeof(Survival_Camp))]
+  class YourPatch1
+  {
+    [HarmonyPrefix]
+    [HarmonyPatch("AttemptCamp")]
+    static bool Prefix(ref GameObject Actor, ref bool __result, ref Survival_Camp __instance)
     {
-        [HarmonyPrefix]
-        [HarmonyPatch("AttemptCamp")]
-        static bool Prefix(ref GameObject Actor, ref bool __result, ref Survival_Camp __instance)
+      if (Actor.AreHostilesNearby())
+      {
+        __result = Actor.Fail("You can't cook with hostiles nearby.");
+        return false;
+      }
+      if (Actor.OnWorldMap())
+      {
+        __result = Actor.Fail("You can't cook on the world map.");
+        return false;
+      }
+      if (!Actor.CanChangeMovementMode("Camping", ShowMessage: true, Involuntary: false, AllowTelekinetic: true))
+      {
+        __result = false;
+        return false;
+      }
+      PointOfInterest one = GetPointsOfInterestEvent.GetOne(Actor, "Campfire");
+      if (one != null && one.GetDistanceTo(Actor) <= 24)
+      {
+        GameObject @object = one.Object;
+        if (@object != null)
         {
-            if (Actor.AreHostilesNearby())
+          if (Actor.IsPlayer())
+          {
+            if (one.IsAt(Actor))
             {
-                __result = Actor.Fail("You can't cook with hostiles nearby.");
-                return false;
+              __result = Actor.Fail("There " + @object.Is + " already " + @object.an() + " here.");
+              return false;
             }
-            if (Actor.OnWorldMap())
-            {
-                __result = Actor.Fail("You can't cook on the world map.");
-                return false;
-            }
-            if (!Actor.CanChangeMovementMode("Camping", ShowMessage: true, Involuntary: false, AllowTelekinetic: true))
-            {
-                __result = false;
-                return false;
-            }
-            PointOfInterest one = GetPointsOfInterestEvent.GetOne(Actor, "Campfire");
-            if (one != null && one.GetDistanceTo(Actor) <= 24)
-            {
-                GameObject @object = one.Object;
-                if (@object != null)
-                {
-                    if (Actor.IsPlayer())
-                    {
-                        if (one.IsAt(Actor))
-                        {
-                            __result = Actor.Fail("There " + @object.Is + " already " + @object.an() + " here.");
-                            return false;
-                        }
-                    }
-                }
-            }
-            string text = __instance.PickDirectionS("Make Camp");
-            if (text == null)
-            {
-                __result = false;
-                return false;
-            }
-            Cell cellFromDirection = Actor.CurrentCell.GetCellFromDirection(text);
-            if (cellFromDirection == null || cellFromDirection.ParentZone != Actor.CurrentZone)
-            {
-                __result = Actor.Fail("You can only build a campfire in the same zone you are in.");
-                return false;
-            }
-            if (cellFromDirection.HasObjectWithTag("ExcavatoryTerrainFeature"))
-            {
-                __result = Actor.Fail("There is nothing there you can build a campfire on.");
-                return false;
-            }
-            if (!cellFromDirection.IsEmpty())
-            {
-                __result = Actor.Fail("Something is in the way!");
-                return false;
-            }
-            GameObject gameObject = Campfire.FindExtinguishingPool(cellFromDirection);
-            if (gameObject != null)
-            {
-                __result = Actor.Fail("You cannot start a campfire in " + gameObject.t() + ".");
-                return false;
-            }
-            IComponent<GameObject>.XDidY(Actor, "make", "camp");
-            GameObject gameObject2 = ((!cellFromDirection.ParentZone.ZoneID.StartsWith("ThinWorld")) ? cellFromDirection.AddObject("Campfire") : cellFromDirection.AddObject("BlueCampfire"));
-            if (Actor.IsPlayer())
-            {
-                gameObject2.SetIntProperty("PlayerCampfire", 1);
-            }
-            if (!__instance.CampedZones.Contains(Actor.CurrentZone.ZoneID))
-            {
-                __instance.CampedZones.Add(Actor.CurrentZone.ZoneID);
-            }
-            __result = true;
-            return false;
+          }
         }
+      }
+      string text = __instance.PickDirectionS("Make Camp");
+      if (text == null)
+      {
+        __result = false;
+        return false;
+      }
+      Cell cellFromDirection = Actor.CurrentCell.GetCellFromDirection(text);
+      if (cellFromDirection == null || cellFromDirection.ParentZone != Actor.CurrentZone)
+      {
+        __result = Actor.Fail("You can only build a campfire in the same zone you are in.");
+        return false;
+      }
+      if (cellFromDirection.HasObjectWithTag("ExcavatoryTerrainFeature"))
+      {
+        __result = Actor.Fail("There is nothing there you can build a campfire on.");
+        return false;
+      }
+      if (!cellFromDirection.IsEmpty())
+      {
+        __result = Actor.Fail("Something is in the way!");
+        return false;
+      }
+      GameObject gameObject = Campfire.FindExtinguishingPool(cellFromDirection);
+      if (gameObject != null)
+      {
+        __result = Actor.Fail("You cannot start a campfire in " + gameObject.t() + ".");
+        return false;
+      }
+      IComponent<GameObject>.XDidY(Actor, "make", "camp");
+      GameObject gameObject2 = ((!cellFromDirection.ParentZone.ZoneID.StartsWith("ThinWorld")) ? cellFromDirection.AddObject("Campfire") : cellFromDirection.AddObject("BlueCampfire"));
+      if (Actor.IsPlayer())
+      {
+        gameObject2.SetIntProperty("PlayerCampfire", 1);
+      }
+      if (!__instance.CampedZones.Contains(Actor.CurrentZone.ZoneID))
+      {
+        __instance.CampedZones.Add(Actor.CurrentZone.ZoneID);
+      }
+      __result = true;
+      return false;
     }
+  }
 }
