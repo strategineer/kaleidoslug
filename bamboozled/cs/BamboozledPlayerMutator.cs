f0b9682a5ce95d0093a31735e5fae385e9198fd1
Bill Tyros <me@strategineer.com>
2024-03-06 01:47:11 -0500

    Add mod to be permanently confused AKA bamboozled, should be fun

---
+++ bamboozled/cs/BamboozledPlayerMutator.cs
@@ -0,0 +1,31 @@
+using System;
+
+using XRL; // to abbreviate XRL.PlayerMutator and XRL.IPlayerMutator
+using XRL.World; // to abbreviate XRL.World.GameObject
+using XRL.World.Effects;
+
+namespace Strategineer.BamboozleMode
+{
+  [Serializable]
+  public class Bamboozled : Confused
+  {
+    public Bamboozled()
+      : base(Int32.MaxValue, 1, 1)
+    {
+      this.DisplayName = "{{K-C-m-M-r-c-CK|bamboozled}}";
+    }
+    public override int GetEffectType()
+    {
+      // same as confused but not removable (I think this does that?)
+      return 83886082;
+    }
+  }
+  [PlayerMutator]
+  public class BamboozledPlayerMutator : IPlayerMutator
+  {
+    public void mutate(GameObject player)
+    {
+      player.ApplyEffect(new Bamboozled());
+    }
+  }
+}
